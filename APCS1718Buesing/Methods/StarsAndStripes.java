b564be7ae5ee42c18eb380ad515b01e2db13d275
BuesingA <31742835+BuesingA@users.noreply.github.com>
2017-10-09 13:18:37 -0500

    Add files via upload

---
+++ APCS1718Buesing/Methods/StarsAndStripes.java
@@ -0,0 +1,31 @@
+package Methods;
+
+import static java.lang.System.*;
+
+public class StarsAndStripes {
+	public StarsAndStripes()
+	   {
+	      out.println("StarsAndStripes");
+	      printTwoBlankLines();
+	   }
+
+	   public void printTwentyStars()
+	   {
+	   }
+
+	   public void printTwentyDashes()
+	   {
+	   }
+
+	   public void printTwoBlankLines()
+	   {
+	   }
+	   
+	   public void printASmallBox()
+	   {	
+	   }
+	 
+	   public void printABigBox()
+	   { 	
+	   }   
+}
