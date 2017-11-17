7c3cd8f17090d1986c109e858ebdcf342e36c8b7
BuesingA <31742835+BuesingA@users.noreply.github.com>
2017-11-17 13:22:54 -0600

    Add files via upload

---
+++ APCS1718Buesing/src/Loops/DigetAdderRunner.java
@@ -0,0 +1,13 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class DigetAdderRunner
+{
+	public static void main( String args[] )
+	{
+		DigetAdder key = new DigetAdder();
+		key.sumDigits(36);
+		
+	}
+}
