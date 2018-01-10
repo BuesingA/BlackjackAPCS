eeb8f640de3ca338d1624fb347ca3ebe10b11f9b
BuesingA <31742835+BuesingA@users.noreply.github.com>
2018-01-10 13:22:10 -0600

    Add files via upload

--- APCS1718Buesing/src/Arrays/MasterRunner.java
+++ APCS1718Buesing/src/Arrays/MasterRunner.java
@@ -5,6 +5,16 @@ public class MasterRunner {
 	public static void main( String args[] )
 	{		
 		Most test= new Most();
-		System.out.println(test.go(new int[] {4,5,7,5,3,6,10}));
+		System.out.println("Most; " + test.go(new int[] {4,5,7,5,3,6,10}));
+		NoRepeat demo = new NoRepeat();
+		System.out.println("No Repeat; " + demo.go(new int[] {4,5,7,5,3,6,10}));
+		RayFirst demo2= new RayFirst();
+		System.out.println("Ray First; " + demo2.go(new int[] {4,5,7,5,3,6,10}));
+		RaySmallest test2= new RaySmallest();
+		System.out.println("Ray Smallest; " + test2.go(new int[] {4,5,7,5,3,6,10}));
+		RaySumLast test3= new RaySumLast();
+		System.out.println("Ray Sum Last; " + test3.go(new int[] {4,5,7,5,3,6,10}));
+		GoingDown test4= new GoingDown();
+		System.out.println("Going Down; " + test4.go(new int[] {4,5,7,5,3,6,10}));
 	}
 }
--- APCS1718Buesing/src/Arrays/NoRepeat.java
+++ APCS1718Buesing/src/Arrays/NoRepeat.java
@@ -2,4 +2,17 @@ package Arrays;
 
 public class NoRepeat {
 
+	public static boolean go(int[] ray)
+	{	
+		for(int i=0; i<ray.length; i++)
+		{
+			for(int j=0; j<ray.length; j++)
+			{
+				if(ray[i]==ray[j])
+					return false;
+			}
+		}
+		
+		return true;
+	}
 }
