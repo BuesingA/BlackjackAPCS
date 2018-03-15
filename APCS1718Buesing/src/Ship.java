64d58194fcf99a5e73d86ca37a1534eb8e220ba8
BuesingA <31742835+BuesingA@users.noreply.github.com>
2018-03-15 13:23:28 -0500

    Add files via upload

--- APCS1718Buesing/src/Ship.java
+++ APCS1718Buesing/src/Ship.java
@@ -2,4 +2,27 @@ package inheritance;
 
 public class Ship {
 
+	public Ship() {	
+	}
+	private String name;
+	private int year;
+	public Ship(String n, int y) {
+		name=n;
+		year=y;
+	}
+	private String getName() {
+		return name;
+	}
+	private int getYear() {
+		reurn year;
+	}
+	private void setName(String n) {
+		name=n;
+	}
+	private void setYear(int y) {
+		year=y;
+	}
+	public String toString() {
+		return (" ");
+	}
 }
